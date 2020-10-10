8f0a272a9531d11145041a440a18c2e93542f22b
clueless-bachu <vasista1997@gmail.com>
2020-10-10 05:30:39 -0400

    Added the delay function to js file

--- kuiper-api/static/js/keyProcess.js
+++ kuiper-api/static/js/keyProcess.js
@@ -1,7 +1,10 @@
 window.addEventListener("keydown", checkKeyPress, false);
 window.addEventListener("keyup", resetKey, false);
 
-urll = "http://35.236.229.125/mission_key"
+// urll = "http://35.236.229.125/mission_key"
+ urll = "http://127.0.0.1:5000/mission_key"
+var d = new Date();
+var process_time = d.getTime();
 function changeText(letter)
 {
 	document.getElementById("key").innerHTML = (letter);
@@ -9,6 +12,8 @@ function changeText(letter)
 
 function checkKeyPress(key)
 {
+	var d = new Date();
+	var n = d.getTime();
 	var letter = key.keyCode;
 	if(letter =="65")
 	{
@@ -27,26 +32,39 @@ function checkKeyPress(key)
 		changeText("s");
 	}
 
-	$.ajax({
+	console.log(n - process_time);
+	if(n - process_time>1000) {
+		$.ajax({
 		url: urll,
             type: "POST",
             contentType: "application/json",
             data: JSON.stringify({"letter": letter})
         		}).done(function(data) {
-            console.log(data); 
+            // console.log(data); 
            });
+        process_time = n;
+	}
+	
+
+     // setTimeout(pollDOM, 300);
 
 };
 
 function resetKey(key) {
 	changeText(" ")
-
 	$.ajax({
 		url: urll,
             type: "POST",
             contentType: "application/json",
             data: JSON.stringify({"letter": "0"})
         		}).done(function(data) {
-            console.log(data); 
+            // console.log(data); 
            });
+
+    
+};
+
+
+function pollDOM() {
+;
 };
